2334143e88217e908a0a413bb1b3ec1b3f62a24c
EdvinEnglund <edvingeenglund@gmail.com>
2026-02-05 10:17:28 +0100

    k-fold evaluation

--- EdvinRForest.py
+++ EdvinRForest.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import (accuracy_score,
                              f1_score,
                              roc_auc_score,
@@ -41,29 +41,69 @@ x = df.drop(columns=cols_to_drop)
 y = df['increase_stock']
 #convert -1 to 0
 y = (y == 1).astype(int)
-#split data
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
+
 #define and fit model
-model = (RandomForestClassifier(
+model = RandomForestClassifier(
     criterion="entropy",
     min_samples_leaf=1,
-    n_estimators=1000
-).fit(x_train, y_train))
-#predict probabiilities
-prob = model.predict_proba(x_test)[:,1]
+    n_estimators=500
+)
+
+#split data
+#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
+
+#create K fold indicies
+n_splits = 10
+kf = KFold(n_splits=10, shuffle=True, random_state=42)
 #adjust probability threshhold
-r = 0.3
-pred = (prob >= r).astype(int)
+r = 0.35
+
+#set evaluation metrics
+accuracy = 0
+f1 = 0
+roc_auc = 0
+pr_auc =0
+precision = 0
+recall = 0
+
+for train_index, test_index in kf.split(x):
+    x_train, x_test = x.iloc[train_index], x.iloc[test_index]
+    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+    model.fit(x_train, y_train)
+
+    # predict probabilities
+    prob = model.predict_proba(x_test)[:, 1]
+    pred = (prob >= r).astype(int)
+
+    # accumulate scores
+    f1 += f1_score(y_test, pred)
+    accuracy += accuracy_score(y_test, pred)
+    roc_auc += roc_auc_score(y_test, pred)
+    pr_auc += average_precision_score(y_test, pred)
+    precision += precision_score(y_test, pred)
+    recall += recall_score(y_test, pred)
+
+print(f"Accuracy (r={r}): {accuracy / n_splits}")
+print(f"F1 (r={r}):  {f1 / n_splits}")
+print(f"Precision (r={r}):  {precision / n_splits}")
+print(f"Recall (r={r}): {recall / n_splits}")
+print(f"ROC AUC: ", roc_auc / n_splits)
+print(f"PR AUC: ", pr_auc / n_splits)
+
+
 #plot au-roc
-fpr, tpr, _ = roc_curve(y_test, prob, pos_label = 1)
-plt.plot(fpr, tpr)
+#fpr, tpr, _ = roc_curve(y_test, prob, pos_label = 1)
+#plt.plot(fpr, tpr)
+
+"""
 #create confusion matrix
 cf = pd.crosstab(pred, y_test, rownames=['Actual'], colnames=['Predicted'])
 print(f"missclassification rate: {1 - accuracy_score(y_test, pred)}")
-print(cf)
 print(f"f1 score = {f1_score(y_test, pred)}")
 print(f"roc auc = {roc_auc_score(y_test, pred)}")
 print(f"pr-auc = {average_precision_score(y_test, pred)}")
 print(f"precision = {precision_score(y_test, pred)}")
 print(f"recall = {recall_score(y_test, pred)}")
+"""
+
 plt.show()
