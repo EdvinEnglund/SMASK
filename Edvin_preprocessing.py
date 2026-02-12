e313ab5b0bd91a400e1996071727d33ee601344e
EdvinEnglund <edvingeenglund@gmail.com>
2026-02-12 12:32:03 +0100

    add new train/test-split

--- Edvin_preprocessing.py
+++ Edvin_preprocessing.py
@@ -1,41 +1,30 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-pd.options.display.max_columns = None
-pd.options.display.max_rows = None
 df = pd.read_csv('training_data_VT2026.csv')
 
-#rebrand output labels
 mapping = {"low_bike_demand": -1, "high_bike_demand": 1}
 df["increase_stock"] = df["increase_stock"].map(mapping)
 
-#drop unnecessary features
-df = df.drop(["snow"], axis = 1)
+df = df.drop(["snow"], axis=1)
 
-#make snowdepth categorical:
 df["snowdepth"] = df["snowdepth"].apply(lambda x: 1 if x > 0 else 0)
 
-#one-hot encoding dummy variables
 cat_cols = ["month", "day_of_week", "hour_of_day"]
-df = pd.get_dummies(
-    df,
-    columns=cat_cols,
-    prefix=cat_cols,
-    dtype=int
-)
+df = pd.get_dummies(df, columns=cat_cols, prefix=cat_cols, dtype=int)
 
-#min-max scale numerical features
-def min_max_scale(df, columns):
-    for column in columns:
-        min_val = df[column].min()
-        max_val = df[column].max()
-        df[column] = (df[column] - min_val) / (max_val - min_val)
-
-scaling_columns = ["dew", "temp", "humidity", "precip", "windspeed", "cloudcover"]
-min_max_scale(df, scaling_columns)
-
+# SPLIT FIRST
 train_df, test_df = train_test_split(df, test_size=0.3, random_state=42)
 
+# SCALE USING TRAIN STATISTICS ONLY
+scaling_columns = ["dew", "temp", "humidity", "precip", "windspeed", "cloudcover", "visibility"]
 
-train_df.to_csv('full_preprocessed_training_data.csv', index = False)
-test_df.to_csv('full_preprocessed_testing_data.csv', index = False)
+for column in scaling_columns:
+    min_val = train_df[column].min()
+    max_val = train_df[column].max()
+
+    train_df[column] = (train_df[column] - min_val) / (max_val - min_val)
+    test_df[column] = (test_df[column] - min_val) / (max_val - min_val)
+
+train_df.to_csv('new_preprocessed_training_data.csv', index=False)
+test_df.to_csv('new_preprocessed_testing_data.csv', index=False)
